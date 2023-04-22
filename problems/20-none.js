57d06db1b36b15e8bf62029f1475258477ef1395
Srivatsan <vatsan1993@gmail.com>
2023-04-22 15:49:14 +0530

    finished 20th

--- problems/20-none.js
+++ problems/20-none.js
@@ -28,8 +28,13 @@ let result4 = none([4, -5, 7, -1], function(n) {
 console.log(result4);   // false
 *******************************************************************************/
 
-let none = function() {
-
+let none = function(arr, cb) {
+    for(let val of arr){
+        if(cb(val)){
+            return false;
+        }
+    }
+    return true;
 };
 
 

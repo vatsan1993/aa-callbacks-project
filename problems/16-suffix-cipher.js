baaf0e0fc0a56939754a12a75219e0865a34f8b1
Srivatsan <vatsan1993@gmail.com>
2023-04-22 15:26:44 +0530

    finished 16th

--- problems/16-suffix-cipher.js
+++ problems/16-suffix-cipher.js
@@ -31,13 +31,45 @@ console.log(suffixCipher('incremental progress is very instrumental', cipher2));
 // INCREMENTAL progressth isth very INSTRUMENTAL
 *******************************************************************************/
 
-let suffixCipher = function() {
-
+let suffixCipher = function(sentence, cbs) {
+    let suffices = Object.keys(cbs);
+    let words = sentence.split(" ");
+    for(let i = 0; i < words.length;  i++){
+        let word = words[i];
+        for(let j = 0; j < suffices.length ; j++){
+            let suffix = suffices[j];
+            let cb = cbs[suffix];
+            if(word.endsWith(suffix)){
+                words[i] = cb(word);
+                break;
+            }
+        }
+    }
+    return words.join(" ");
 };
 
 
 
+let cipher1 = {
+    ly: function(word) {
+        return word.slice(0, -1) + 'ee';
+    },
+    ize: function(word) {
+        return word + 'r';
+    }
+};
+console.log(suffixCipher('quietly and gently visualize', cipher1));
+// quietlee and gentlee visualizer
 
+let cipher2 = {
+    tal: function(word) {
+        return word.toUpperCase();
+    },
+    s: function(word) {
+        return word + 'th';
+    }
+};
+console.log(suffixCipher('incremental progress is very instrumental', cipher2));
 
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/

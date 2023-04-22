0dcbe7eb950969c947e4bb3210de2b1a0a21d569
Srivatsan <vatsan1993@gmail.com>
2023-04-22 15:58:43 +0530

    finished 22

--- problems/22-first-index.js
+++ problems/22-first-index.js
@@ -22,8 +22,14 @@ let result3 = firstIndex(['canine', 'feline', 'tree'], function (s) {
 console.log(result3); // -1
 *******************************************************************************/
 
-let firstIndex = function() {
-
+let firstIndex = function(arr, cb) {
+    for(let i =0; i< arr.length; i++){
+        let val = arr[i];
+        if(cb(val)){
+            return i;
+        }
+    }
+    return -1;
 };
 
 

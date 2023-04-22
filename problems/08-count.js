6ad74a10816969b95f2844bc619ae27656fe375b
Srivatsan <vatsan1993@gmail.com>
2023-04-22 13:56:56 +0530

    8th problem

--- problems/08-count.js
+++ problems/08-count.js
@@ -26,8 +26,15 @@ let result4 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str)
 console.log(result4); // 0
 *******************************************************************************/
 
-let count = function() {
-
+let count = function(arr, cb) {
+    let count = 0;
+    for(let i = 0 ;i < arr.length; i++){
+        let val = arr[i];
+        if(cb(val)){
+            count++;
+        }
+    }
+    return count;
 };
 
 

6dfa21f1ecb257fc034c72b449d3f7265858d2f6
Srivatsan <vatsan1993@gmail.com>
2023-04-22 14:37:19 +0530

    finished 15th

--- problems/15-sentence-mapper.js
+++ problems/15-sentence-mapper.js
@@ -25,8 +25,12 @@ let result2 = sentenceMapper("this is pretty cool right", removeVowels);
 console.log(result2); // 'ths s prtty cl rght'
 *******************************************************************************/
 
-let sentenceMapper = function() {
-
+let sentenceMapper = function(str, cb) {
+    let words = str.split(" ")
+    for(let i =0; i < words.length; i++){
+        words[i] = cb(words[i]);
+    }
+    return words.join(" ");
 };
 
 
